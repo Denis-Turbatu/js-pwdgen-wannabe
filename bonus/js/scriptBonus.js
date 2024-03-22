08971b3c1cd008f8becc5c4dee64ed99d7e1f84a
Denis-Turbatu <turbatudenis81@gmail.com>
2024-03-22 13:26:24 +0100

    feat: create bonus folder with html,css,js files and bonus

---
+++ bonus/js/scriptBonus.js
@@ -0,0 +1,10 @@
+const username = prompt("Come ti chiami?");
+console.log(username);
+
+const surname = prompt("Qual è il tuo cognome?");
+console.log(surname);
+
+const colPref = prompt("Qual è il tuo colere preferito?");
+console.log(colPref);
+
+document.getElementById('result').innerHTML = username + surname + colPref + Math.floor(Math.random() * 100);
